bdb1b1c1059b7b20ba4f1f59d9451b2f9fa4f0d9
jodosjodos <jeandedieu2030@gmail.com>
2024-02-02 01:02:04 +0200

    done with login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,14 @@ import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { NewUserDTO } from 'src/user/dtos/new-user.dto';
 import { UserDetails } from 'src/user/userDetails.interface';
+import { ExistingUserDto } from 'src/user/dtos/existing-user.dto';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
   async hashPassword(password: string): Promise<string> {
     return bcrypt.hash(password, 12);
   }
@@ -22,5 +27,40 @@ export class AuthService {
       hashedPassword,
     );
     return this.userService._getUserDetails(newUSer);
+  }
+
+  //   login
+  async doesPasswordMatch(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<UserDetails | null> {
+    const user = await this.userService.findByEmail(email);
+    const doesUserExist = !!user;
+    if (!doesUserExist) {
+      return null;
+    }
+    const doesPasswordMatch = await this.doesPasswordMatch(
+      password,
+      user.password,
+    );
+    if (!doesPasswordMatch) return null;
+    return this.userService._getUserDetails(user);
+  }
+
+  async login(
+    existingUser: ExistingUserDto,
+  ): Promise<{ token: string } | null> {
+    const { email, password } = existingUser;
+    const user = await this.validateUser(email, password);
+    if (!user) return null;
+    const jwt = await this.jwtService.signAsync({ user });
+    return { token: jwt };
   }
 }
