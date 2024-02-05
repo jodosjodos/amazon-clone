989e651a83562b49e75e2713c215c06beed19cbd
jodosjodos <jeandedieu2030@gmail.com>
2024-02-05 07:13:05 +0200

    ad coment

--- amazon/src/features/auth/AuthLayout.tsx
+++ amazon/src/features/auth/AuthLayout.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <Grid>
+      {/* grid */}
       <img src="amazon-logo.png" alt="amazon logo"  />
       <main>{children}</main>
     </Grid>
