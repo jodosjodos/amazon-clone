c5351f4bca27d46ece06e77dfb44595fa7bc794d
jodosjodos <jeandedieu2030@gmail.com>
2024-02-08 21:06:12 +0200

    done with custom hooks

--- amazon/src/shared/utils/validation/length.ts
+++ amazon/src/shared/utils/validation/length.ts
@@ -1,7 +1,7 @@
 import { LengthOptions } from "./models/options/length";
 import { ValidatorFn } from "./models/validatorFn";
 
-const _validateLenth: ValidatorFn = (
+export const _validateLength: ValidatorFn = (
   text: string,
   options?: LengthOptions
 ): boolean => {
@@ -9,4 +9,14 @@ const _validateLenth: ValidatorFn = (
   if (options?.min && textLength < options?.min) return false;
   if (options?.max && textLength > options?.max) return false;
   return true;
+};
+
+// validate password
+export const validatePasswordLength = (text: string): boolean => {
+  return _validateLength(text, { min: 6, max: 20 });
+};
+
+// validate name
+export const validateNameLength = (text: string): boolean => {
+  return _validateLength(text, { min: 2 });
 };
