9eb58488ec3464438195e02c59cf48fb8c6026cd
jodosjodos <jeandedieu2030@gmail.com>
2024-02-08 10:50:01 +0200

    fix logo problems

--- amazon/src/features/auth/AuthLayout.tsx
+++ amazon/src/features/auth/AuthLayout.tsx
@@ -3,9 +3,9 @@ import { ReactNode } from "react";
 
 function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <Grid>
+    <Grid sx={{p:2}} container direction="column" justifyContent="flex-start" alignItems="center" >
       {/* grid */}
-      <img src="amazon-logo.png" alt="amazon logo"  />
+      <img src="amazon-logo.png" alt="amazon logo" height="40px"  />
       <main>{children}</main>
     </Grid>
   );
